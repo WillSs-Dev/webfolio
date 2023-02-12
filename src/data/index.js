fab341a9413ac0ed4bd1e6c3ab4d2843bdaef154
Will <williansilva3dsi@hotmail.com>
2023-02-12 20:36:12 -0300

    feat: adding initial data for project

---
+++ src/data/index.js
@@ -0,0 +1,17 @@
+export const projects = [
+  {
+    name: "MKS's Store",
+    thumbnail: "https://i.ibb.co/Vm6HJRp/Screenshot-4.png",
+    skills: ["react", "redux", "styled-components"],
+  },
+  {
+    name: "Gaming Store",
+    thumbnail: "https://i.ibb.co/445Mwf0/Screenshot-5.png",
+    skills: ["react", "redux", "mui", "react-router-dom"],
+  },
+  {
+    name: "Pixel Art Canvas",
+    thumbnail: "https://i.ibb.co/7zG04Wz/Screenshot-6.png",
+    skills: ["html", "css", "javascript"],
+  },
+];
